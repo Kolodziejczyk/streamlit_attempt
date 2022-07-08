00554f830e3a87643b470be405800c23cd5eef50
Kolodziejczyk <kolodziejczyk@rocketmail.com>
2022-07-08 13:22:11 +0200

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit
+import pandas
+import snowflake.connector
+import requests
+from urlib.error import URLError
+
+streamlit.tiltle('Zdrowe sniadanie')
+
+#my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"
